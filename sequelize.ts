65d8a79331f36fc909463365a6eb8245b98d27ab
Vladimir Halme <vhalme@gmail.com>
2021-03-29 19:13:40 +0300

    sequelize native true

--- sequelize.ts
+++ sequelize.ts
@@ -10,10 +10,18 @@ const sequelize = new Sequelize(dbUrl, {
   dialectOptions: {
     ssl: true
   },
+  native: true,
   logging: console.log,
   define: {
     freezeTableName: true,
   }
 });
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 export default sequelize;
